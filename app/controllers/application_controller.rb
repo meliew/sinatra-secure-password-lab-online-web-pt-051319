e1d06ced087e0a8102558325c23bdfb1443ca3b4
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-23 00:45:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,8 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     if params[:username] == "" || params[:password] == ""
-        redirect "/failure"'"
-    else User.create(:username => params[:username], :password => params[:password])
+        redirect "/failure"
+    else @user = User.create(:username => params[:username], :password => params[:password])
       redirect "/login"
 
 
